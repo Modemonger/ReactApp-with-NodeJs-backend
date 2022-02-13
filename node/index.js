2f48127265756fc2392b2fa58d503ef4436706d8
Modemonger <sta.modestas@gmail.com>
2022-02-13 23:16:54 +0200

    get post and delete api finished

--- node/index.js
+++ node/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser= require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 const port = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({extended: false}));
 
 const connectionString = process.env.API_KEY;
 
@@ -18,11 +17,13 @@ MongoClient.connect(connectionString)
         const quotesCollection = db.collection('quotes')
 
         app.post('/post', (req, res) => {
+            console.log(req.body, " Post request")
             quotesCollection.insertOne(req.body)
             .then(result => {
-                res.redirect('/')
+                res.send('Quote recieved. Object Id is '+result.insertedId);
             })
             .catch(error => console.error(error))
+            
         });
         
         app.get('/quotes', async (req, res) => {
@@ -33,10 +34,22 @@ MongoClient.connect(connectionString)
             }
         });
 
-        app.get("/api", (req, res) => {
+        app.get('/api', (req, res) => {
             console.log('Receaved api request')
             res.json({ message: "Hello from the server side!" });
-          });
+        });
+
+        app.delete('/remove:quote', (req, res) => {
+            const reqQuote = req.params.quote.slice(1);
+            console.log('Removing quote ',reqQuote);
+            quotesCollection.deleteOne({ quote: reqQuote })
+            .then(result => {
+                console.log(result);
+                res.send('Quote: "'+reqQuote+'" has been removed')
+            })
+            .catch(error => console.error(error))
+            
+        });
 
       })
       .catch(error => console.error(error))
--- react-node-app/src/App.js
+++ react-node-app/src/App.js
@@ -1,37 +1,77 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 
 function App() {
 
   const [data,setData] = useState(null);
   const [quotes,setQuotes] = useState(null);
+  const [name, setName] = useState('');
+  const [quote, setQuote] = useState('');
+
+  //Get data from app
+  function RetrieveData(){
+      axios.get('/api')
+        .then(res => res.data)
+        .then((data) => setData(data.message))
+        .catch(err => console.log(err.data))
+    
+      axios.get('/quotes')
+        .then(res => res.data)
+        .then((data) => {setQuotes(data)});
+  }
 
   useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    RetrieveData()
   }, [])
   
-  useEffect(() => {
-    fetch('/quotes')
-      .then((res) => res.json())
-      .then((data) => {
-        setQuotes(data);
-      });
-  }, [])
+  
+
+  //send data to api
+  function sendPost(e, post){
+    e.preventDefault();
+      axios.post('/post', post)
+      .then(response => response.data)
+      .then(data => console.log(data))
+      .then(RetrieveData())
+      .catch(err => console.log(err.data))
+      
+    setQuote('');
+    setName('');
+  }
+
+  function removeQuote(e, quote){
+    e.preventDefault();
+    console.log('Removing');
+    axios.delete(`/remove:${quote}`)
+    .then(response => console.log(response.data))
+    .then(RetrieveData())
+    .catch(err => console.log(err.data))
+    
+    
+  }
+  
 
   const printQuotes = () => {
     let output =[];
     quotes.forEach(quote => {
-      output.push(<p>{quote.name}: {quote.quote}</p>);
+      output.push(<p key={quote._id}>{quote.name}: {quote.quote} <input type='button' value='X' onClick={(event) => removeQuote(event, quote.quote)} /> </p>);
     });
     return output;
   }
 
+  
+ 
   return (
     <div className="App">
       <header className="App-header">
         <p>{!data ? "Loading..." : data}</p>
+        <form onSubmit={(event) => sendPost(event, {name: name, quote: quote})}>
+          <input type='text' value={name} onChange={((e) => {setName(e.target.value)})} />
+          <input type='text' value={quote} onChange={((e) => {setQuote(e.target.value)})} />
+          <input type='submit' value="Submit" />
+        </form>
+        
         {!quotes ? 'Loading...' : printQuotes()}
       </header>
     </div>
